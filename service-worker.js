9bc83b3130321555de8a5de8be127871367e0b95
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2019-11-03 15:40:06 +0100

    Add maxlength for input field

--- service-worker.js
+++ service-worker.js
@@ -11,12 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -28,72 +26,72 @@ self.addEventListener("message", event => {
  */
 self.__precacheManifest = [
   {
-    url: "404.html",
-    revision: "838253da49fc5206fe386693fc7e8c7d"
+    "url": "404.html",
+    "revision": "838253da49fc5206fe386693fc7e8c7d"
   },
   {
-    url: "ime/index.html",
-    revision: "06082a0d015cd54e77abf83ef64f1a9d"
+    "url": "ime/index.html",
+    "revision": "3aba69243f666ba01407f67643b4f315"
   },
   {
-    url: "ime/js/index.js",
-    revision: "ec534caff28e4d0ba07c7f33f5edbe2e"
+    "url": "ime/js/index.js",
+    "revision": "ec534caff28e4d0ba07c7f33f5edbe2e"
   },
   {
-    url: "img/abjad-og.png",
-    revision: "5d2f8e2cf757fae3d1e8110ae03d06ad"
+    "url": "img/abjad-og.png",
+    "revision": "5d2f8e2cf757fae3d1e8110ae03d06ad"
   },
   {
-    url: "img/app-icon-144.png",
-    revision: "d6665d6631c138e294b0d95dfaeb28d3"
+    "url": "img/app-icon-144.png",
+    "revision": "d6665d6631c138e294b0d95dfaeb28d3"
   },
   {
-    url: "img/app-icon-168.png",
-    revision: "daa345dc3874f714480844940085fb96"
+    "url": "img/app-icon-168.png",
+    "revision": "daa345dc3874f714480844940085fb96"
   },
   {
-    url: "img/app-icon-192.png",
-    revision: "33408fac6c6a2dcad0a09aee7659d08b"
+    "url": "img/app-icon-192.png",
+    "revision": "33408fac6c6a2dcad0a09aee7659d08b"
   },
   {
-    url: "img/app-icon-48.png",
-    revision: "effa442a735727f2c38951a3cd97c0c7"
+    "url": "img/app-icon-48.png",
+    "revision": "effa442a735727f2c38951a3cd97c0c7"
   },
   {
-    url: "img/app-icon-512.png",
-    revision: "62991216ee00dcd64d574fbcea15ceb4"
+    "url": "img/app-icon-512.png",
+    "revision": "62991216ee00dcd64d574fbcea15ceb4"
   },
   {
-    url: "img/app-icon-72.png",
-    revision: "6caeef59c5e426f3f217329ace0074d7"
+    "url": "img/app-icon-72.png",
+    "revision": "6caeef59c5e426f3f217329ace0074d7"
   },
   {
-    url: "img/app-icon-96.png",
-    revision: "585cbb881f53bcf2f81fb479b65273b9"
+    "url": "img/app-icon-96.png",
+    "revision": "585cbb881f53bcf2f81fb479b65273b9"
   },
   {
-    url: "img/icon-192.png",
-    revision: "efbcda7eef1a5cbd542ed59b432332fe"
+    "url": "img/icon-192.png",
+    "revision": "efbcda7eef1a5cbd542ed59b432332fe"
   },
   {
-    url: "img/icon.png",
-    revision: "f8601950f47572166a5eb040403da07b"
+    "url": "img/icon.png",
+    "revision": "f8601950f47572166a5eb040403da07b"
   },
   {
-    url: "index.html",
-    revision: "dcd1a3177f07af11b85a12f0600ae8f0"
+    "url": "index.html",
+    "revision": "87b99446ade87cc1859f400746e9c8ff"
   },
   {
-    url: "js/index.js",
-    revision: "a7de1e61eb1d40159bd9e219ee9f626f"
+    "url": "js/index.js",
+    "revision": "a7de1e61eb1d40159bd9e219ee9f626f"
   },
   {
-    url: "manifest.json",
-    revision: "f18bc8a4facb5688889ba5c8af6b1754"
+    "url": "manifest.json",
+    "revision": "f18bc8a4facb5688889ba5c8af6b1754"
   },
   {
-    url: "table/index.html",
-    revision: "3823219a5799481238d2cb1c16e33cd1"
+    "url": "table/index.html",
+    "revision": "ce34aaf6aecd1a302d1d79c3e3979394"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
