aac638c546d181db2a80bcb9d57efe38d42d61c7
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2022-04-06 15:50:36 +0200

    Bump deps

--- build.sh
+++ build.sh
@@ -3,10 +3,9 @@
 set -Eeuo pipefail
 
 sort-package-json &&
-	npm install &&
-	npm run prettify &&
-	npm run standardize &&
-	npm run check &&
-	npm run compile-main &&
-	npm run compile-ime &&
-	npm run lint-css
+	pnpm i &&
+	dum format &&
+	dum lint &&
+	dum check &&
+	dum compile-main &&
+	dum compile-ime
