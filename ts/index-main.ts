3cdc56671a259c9c828c8547c6f5190f5c576b3b
Theo Beers <theo.beers@fu-berlin.de>
2026-05-12 16:46:15 -0400

    update dependencies

--- ts/index-main.ts
+++ ts/index-main.ts
@@ -9,20 +9,12 @@ const inputField = document.getElementById("abjad-text") as HTMLInputElement;
 const resultField = document.getElementById("result") as HTMLElement;
 
 // Checkboxes
-const maghribiCheckbox = document.getElementById(
-  "maghribi-check",
-) as HTMLInputElement;
-const hamzahCheckbox = document.getElementById(
-  "hamzah-check",
-) as HTMLInputElement;
+const maghribiCheckbox = document.getElementById("maghribi-check") as HTMLInputElement;
+const hamzahCheckbox = document.getElementById("hamzah-check") as HTMLInputElement;
 
 // Buttons
-const submitButton = document.getElementById(
-  "submit-button",
-) as HTMLButtonElement;
-const resetButton = document.getElementById(
-  "reset-button",
-) as HTMLButtonElement;
+const submitButton = document.getElementById("submit-button") as HTMLButtonElement;
+const resetButton = document.getElementById("reset-button") as HTMLButtonElement;
 
 //
 // EVENT HANDLING
